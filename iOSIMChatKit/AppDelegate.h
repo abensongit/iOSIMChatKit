2cd3b7ed550bda3864b9402e33af4cd0fe9574ff
jetter <jetter@fy.com>
2020-02-25 19:20:08 +0800

    Initial Commit

---
+++ iOSIMChatKit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSIMChatKit
+//
+//  Created by fangyuan on 2020/2/25.
+//  Copyright © 2020 iosimkit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ iOSIMChatKit/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  iOSIMChatKit
+//
+//  Created by fangyuan on 2020/2/25.
+//  Copyright © 2020 iosimkit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
